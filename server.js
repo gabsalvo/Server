35e79ca8c4bcc6f1c90ec6483af94b64e9a258a6
Gabriele <g.salvo4@studenti.unipi.it>
2023-10-27 00:28:10 +0200

    Aggiunta Compilazione

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { exec } = require('child_process');
+const fs = require('fs');
+
 
 const app = express();
 const PORT = 3000;
@@ -12,17 +15,47 @@ app.get('/', (req, res) => {
     res.send('Benvenuto al server!');
 });
 
+app.get('/api/compile', (req, res) => {
+    res.send('Post me pls to get your C code compiled');
+});
+
 // Endpoint api/compile
 app.post('/api/compile', (req, res) => {
     const { code, input, languageId } = req.body;
 
-    // Qui puoi eseguire la compilazione e l'esecuzione del codice...
-    // ... e restituire l'output come risposta.
+    if (languageId !== 'c') { // Assicurati di utilizzare l'ID corretto per il linguaggio C
+        return res.json({
+            success: false,
+            output: 'Linguaggio non supportato'
+        });
+    }
 
-    // Per ora, restituiamo un semplice messaggio come esempio:
-    res.json({
-        success: true,
-        output: `Il tuo codice è: ${code}. Input fornito: ${input}. Linguaggio ID: ${languageId}`
+    // Scrivi il codice in un file temporaneo
+    fs.writeFileSync('temp.c', code);
+
+    // Compila il codice
+    exec('gcc temp.c -o temp.out', (err) => {
+        if (err) {
+            return res.json({
+                success: false,
+                output: 'Errore durante la compilazione: ' + err.message
+            });
+        }
+
+        // Esegui il codice compilato
+        exec('./temp.out', { timeout: 5000 }, (err, stdout, stderr) => {
+            if (err) {
+                return res.json({
+                    success: false,
+                    output: `Errore durante l'esecuzione: ` + stderr
+                });
+            }
+            res.json({
+                success: true,
+                output: stdout
+            });
+        });
+        
     });
 });
 
