e9e3a614a5171f69618bde9d40995b8fc1bb8a0b
Gabriele <g.salvo4@studenti.unipi.it>
2023-10-27 23:41:55 +0200

    ripriviamo

--- server.js
+++ server.js
@@ -13,11 +13,9 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "ngrok-skip-browser-warning"],
 };
 
-// Middlewares
 app.use(bodyParser.json());
-app.use(cors(corsOptions)); 
+app.use(cors(corsOptions));
 
-// Endopoint Get
 app.get("/", (req, res) => {
   res.send("Benvenuto al server!");
 });
@@ -26,7 +24,6 @@ app.get("/api/compile", (req, res) => {
   res.send("Post me pls to get your C code compiled");
 });
 
-// Endpoint POST /api/compile
 app.post("/api/compile", (req, res) => {
   const { code, input, languageId } = req.body;
 
@@ -37,44 +34,42 @@ app.post("/api/compile", (req, res) => {
     });
   }
 
-  const fileName = `temp_code`;
+  const fileName = `temp_code.c`;
 
-  fs.writeFileSync(`${fileName}.c`, code);
+  fs.writeFileSync(fileName, code);
 
-  try {
-    exec(`gcc ${fileName}.c -o ${fileName}.out`, (err) => {
+  exec(`gcc ${fileName} -o ${fileName}.out`, (err) => {
+    if (err) {
+      return res.json({
+        success: false,
+        output: "Errore durante la compilazione: " + err.message,
+      });
+    }
+
+    exec(`./${fileName}.out`, { timeout: 5000 }, (err, stdout, stderr) => {
       if (err) {
         return res.json({
           success: false,
-          output: "Errore durante la compilazione: " + err.message,
+          output: `Errore durante l'esecuzione: ` + stderr,
         });
       }
-
-      exec(
-        `./${fileName}.out`,
-        { timeout: 5000 },
-        (err, stdout, stderr) => {
-          if (err) {
-            return res.json({
-              success: false,
-              output: `Errore durante l'esecuzione: ` + stderr,
-            });
-          }
-          res.json({
-            success: true,
-            output: stdout,
-          });
-        }
-      );
+      res.json({
+        success: true,
+        output: stdout,
+      });
     });
-  } finally {
-    fs.unlinkSync(`${fileName}.c`);
-    if (fs.existsSync(`${fileName}.out`)) {
-      fs.unlinkSync(`${fileName}.out`);
-    }
-  }
+  });
 });
 
 app.listen(PORT, () => {
   console.log(`Server in ascolto sulla porta ${PORT}...`);
+});
+
+process.on('exit', () => {
+  if (fs.existsSync('temp_code.c')) {
+    fs.unlinkSync('temp_code.c');
+  }
+  if (fs.existsSync('temp_code.c.out')) {
+    fs.unlinkSync('temp_code.c.out');
+  }
 });
